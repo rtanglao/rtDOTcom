4e4d9b7daf2cb10182325d3dd79f39bdfcc080c7
Roland Tanglao <roland@rolandtanglao.com>
2018-11-01 22:44:47 -0700

    Create 2018-11-01-p1-stevejburr-replicating-flowing-data-population-charts-fascinating-datascience-journey.r

---
+++ _posts/2018-11-01-p1-stevejburr-replicating-flowing-data-population-charts-fascinating-datascience-journey.r
@@ -0,0 +1,10 @@
+---
+layout: post
+title: "Steve Burr's fascinating behind the messy scenes of a data science workflow:Replicating Flowingdata Population Charts in R"
+---
+
+## Pontifications
+
+* Love Steve Burr's fascinating behind the messy scenes of a data science workflow:
+[Replicating Flowingdata Population Chart](https://www.stevejburr.com/post/replicating-flowingdata-population-charts-in-r/)!
+Shows the messiness and the evolution of a data visualization. i.e. the reality :-) !
